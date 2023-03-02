be457b7dbffc2262a58559c5ead5bc914002885c
Edgar Horta <edgar.horta34@my.unitec.edu.mx>
2023-03-01 19:47:45 -0600

    modificacion

---
+++ src/Application/Circulo.cs
@@ -0,0 +1,15 @@
+namespace Application;
+
+public class Cuadrado
+{
+    public static double Perimetro(double lado)
+    {
+        double perimetro = lado + lado + lado + lado;
+        return perimetro;
+    }
+    public static double Area(double lado)
+    {
+        double area = lado * lado;
+        return area;
+    }
+}
